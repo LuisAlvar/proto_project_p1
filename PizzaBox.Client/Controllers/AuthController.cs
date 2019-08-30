7052e37d7f99bd4b00d55e85457d08b33c9fef4d
luis alvarez <alvarez.en.luis@gmail.com>
2019-08-29 22:27:24 -0500

    done with signing up a user

--- PizzaBox.Client/Controllers/AuthController.cs
+++ PizzaBox.Client/Controllers/AuthController.cs
@@ -5,20 +5,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PizzaBox.Client.Models;
+using PizzaBox.Data;
+using PizzaBox.Domain.Models.DbModels;
 
 namespace  PizzBox.Client.Controllers
 {
     public class AuthController : Controller{
+      private PizzaBoxDbContext _db = new PizzaBoxDbContext();
       public IActionResult SignIn(){
         return View();
       }
 
+      [HttpGet]
       public IActionResult SignUp(){
         return View();
       }
 
+      [HttpPost]
+      public IActionResult SignUp(PizzaBox.Domain.Models.DbModels.User newUser){
+        if(ModelState.IsValid){
+          _db.Users.Add(newUser);
+          _db.SaveChanges();
+        }
+
+        return RedirectToAction("SignUp");
+      }
+
       public IActionResult CheckSignIn(){
-        
+
         return RedirectToAction("SignIn");
       }
     }
--- PizzaBox.Domain/Models/DbModels/User.cs
+++ PizzaBox.Domain/Models/DbModels/User.cs
@@ -1,12 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PizzaBox.Domain.Models.DbModels{
   public class User{
     public int Id { get; set; }
+    [Required]
+    public string Username { get; set; }
+    [Required]
     public string FirstName { get; set; }
+    [Required]
     public string LastName { get; set; }
+    [Required]
     public string Address { get; set; }
     public string Address2 { get; set; }
+    [Required]
     public string City { get; set; }
+    [Required]
     public string State {get; set;}
+    [Required]
     public string ZipCode { get; set; }
+    [Required]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; }
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    [Required]
+    [DataType(DataType.PhoneNumber)]
+    public string PhoneNumber { get; set; }
   }
 }
